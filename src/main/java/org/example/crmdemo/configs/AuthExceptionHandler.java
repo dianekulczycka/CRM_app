4a27e66f643e1f42a8942fee72b362a238cea9dd
dina <leshlesh1337@gmail.com>
2025-03-04 19:44:00 +0200

    fix

--- src/main/java/org/example/crmdemo/configs/AuthExceptionHandler.java
+++ src/main/java/org/example/crmdemo/configs/AuthExceptionHandler.java
@@ -1,6 +1,5 @@
 package org.example.crmdemo.configs;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +11,7 @@ import java.io.IOException;
 @Slf4j
 public class AuthExceptionHandler implements AuthenticationEntryPoint {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         log.error(authException.getMessage(), authException);
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
     }
